46c970e460c4927c8e242fda94804469364287a5
JiuChen0 <jiuchen1024@gmail.com>
2024-01-29 11:50:57 -0800

    H7 repo

--- H7/src/main.cpp
+++ H7/src/main.cpp
@@ -1,38 +1,18 @@
 #include <Arduino.h>
-#include "UltrasonicSensors_1.h" // JSN-SR04T sensor
-#include "UltrasonicSensors_2.h" // Taidacent sensor
 
-// Pins for the JSN-SR04T sensor
-const int triggerPinJSN = 7;
-const int echoPinJSN = 6;
-
-// Pins for the Taidacent sensor
-const int triggerPinTaidacent = 9;
-const int pwmPinTaidacent = 8;
-
-// Create instances of the sensor classes
-UltrasonicSensor ultrasonicJSN(triggerPinJSN, echoPinJSN);
-UltrasonicSensor_2 ultrasonicTaidacent(triggerPinTaidacent, pwmPinTaidacent);
+// put function declarations here:
+int myFunction(int, int);
 
 void setup() {
-    Serial.begin(9600);
-    ultrasonicJSN.begin();            // Initialize JSN-SR04T sensor
-    ultrasonicTaidacent.begin();      // Initialize Taidacent sensor
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
 }
 
 void loop() {
-    long distanceJSN = ultrasonicJSN.readDistance();
-    float distanceTaidacent = ultrasonicTaidacent.readDistance();
+  // put your main code here, to run repeatedly:
+}
 
-    Serial.print("JSN-SR04T Distance: ");
-    Serial.print(distanceJSN);
-    Serial.println(" cm");
-
-    Serial.print("Taidacent Distance: ");
-    Serial.print(distanceTaidacent);
-    Serial.println(" cm");
-
-    // Add obstacle avoidance decision logic based on sensor readings here
-
-    delay(100); // Delay a bit to avoid spamming the serial output
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
 }
