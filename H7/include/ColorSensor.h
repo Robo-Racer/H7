6483ad50a0a190390de2fbc2b8b32a9f469b7b8e
bLutz7269 <brycelutz6@gmail.com>
2024-02-20 18:07:50 -0800

    implemented basic color sensor data gathering using the multiplexer

---
+++ H7/include/ColorSensor.h
@@ -0,0 +1,10 @@
+// ColorSensor.h
+
+//For detecting any objects. blind zone distance of 3cm, super low, it has a detection range of 3cm-4.5m
+//https://www.amazon.com/JSN-SR04T-Integrated-Transducer-Ultrasonic-Waterproof/dp/B0C73XZRFQ/ref=sr_1_5?crid=L86R36BB1MQ1&keywords=waterproof+ultrasonic+sensor&qid=1700536333&sprefix=waterproof+ultras%2Caps%2C166&sr=8-5
+
+
+#ifndef COLORSENSOR_H
+#define COLORSENSOR_H
+
+#endif
--- H7/src/main.cpp
+++ H7/src/main.cpp
@@ -1,19 +1,67 @@
 #include <Arduino.h>
+#include "Wire.h"
+#include "Adafruit_APDS9960.h"
+#include "ColorSensor.h"
+
+#define TCAADDR 0x70
+#define COLORSENSOR_SDA -1  //Define Pins for H7
+#define COLORSENSOR_SCL -1  //Define Pins for H7
+
+Adafruit_APDS9960 colorSensor[6];
+TwoWire colorWire(COLORSENSOR_SDA, COLORSENSOR_SCL);
+
 
 // put function declarations here:
-int myFunction(int, int);
+void tcaselect(uint8_t);
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  colorWire.begin();
+  Serial.begin(115200);
+  while (!Serial);  
+
+
+  //initialize all of the color sensors
+  for(uint8_t i = 0; i <= 5; i++)
+    {
+    tcaselect(i);
+    delay(10);
+    colorSensor[i].begin(10, APDS9960_AGAIN_4X, APDS9960_ADDRESS, &colorWire);
+    colorSensor[i].enable(true);
+    colorSensor[i].enableColor(true);
+    }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("Hello World");
+  uint16_t r=0, g=0, b=0, a=0;
+
+  for(uint8_t i = 0; i <= 5; i++)
+    { 
+    tcaselect(i);
+    delay(10);
+    if(colorSensor[i].colorDataReady())
+      {
+      colorSensor[i].getColorData(&r, &g, &b, &a);
+      Serial.print(i);
+      Serial.print(" R=");
+      Serial.print(r);
+      Serial.print(" G=");
+      Serial.print(g);
+      Serial.print(" B=");
+      Serial.print(b);
+      Serial.print(" A=");
+      Serial.println(a);
+      }
+    
+    }
+  Serial.println();
+  delay(5000);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+  colorWire.beginTransmission(TCAADDR);
+  colorWire.write(1 << i);
+  colorWire.endTransmission();  
 }
